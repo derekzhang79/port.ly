f26668a939ee6b8d378b3f3ea8fbb9081086a8f9
Matt Cooper <matthew_cooper@brown.edu>
2016-02-06 19:26:00 -0500

    added main.m

---
+++ Port.ly/Port.ly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Port.ly
+//
+//  Created by Matt Cooper on 2/6/16.
+//  Copyright © 2016 Matthew Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
