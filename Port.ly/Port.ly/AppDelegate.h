c003470e4a6e1abb0ecd4b20b0387a8d0373d77b
Matt Cooper <matthew_cooper@brown.edu>
2016-02-06 19:10:10 -0500

    sd;akfjbn

---
+++ Port.ly/Port.ly/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Port.ly
+//
+//  Created by Matt Cooper on 2/6/16.
+//  Copyright © 2016 Matthew Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Port.ly/Port.ly/RequestVC.h
@@ -0,0 +1,16 @@
+//
+//  RequestVC.h
+//  Port.ly
+//
+//  Created by Matt Cooper on 2/6/16.
+//  Copyright © 2016 Matthew Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface RequestVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
